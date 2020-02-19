ab0d2d6c58388f4d11b725e907496e55a6a39644
hejie <cuihejie331771@gmail.com>
2020-02-19 16:32:56 -0500

    add comments

--- hw2/Apriori.py
+++ hw2/Apriori.py
@@ -1,7 +1,7 @@
 '''
 @Author: Hejie Cui
 @Date: 2020-02-17 18:54:57
-@LastEditTime: 2020-02-19 13:00:35
+@LastEditTime: 2020-02-19 16:32:24
 @FilePath: /CS570-DataMining/hw2/Apriori.py
 '''
 import time
@@ -10,6 +10,7 @@ import itertools
 from functools import cmp_to_key
 
 
+# read the input dataset to a two-dimentional list
 def parse_dataset(input_dataset_name):
     dataset = []
     with open(input_dataset_name, 'r') as input_dataset_file:
@@ -19,12 +20,13 @@ def parse_dataset(input_dataset_name):
     return dataset
 
 
-# function for rerank the frequent itemset to have the same order as example-output.txt
+# get the int item values from each transaction
 def get_values(itemset):
     list_of_item_value = [int(x) for x in itemset]
     return list_of_item_value
 
 
+# comparation function rank two frequent itemsets
 def compare(a, b):
     a = get_values(a)
     a_len = len(a)
@@ -46,6 +48,7 @@ def compare(a, b):
                 return a[i] - b[i]
 
 
+# write the frequent_itemset_dict into file, as the same order as in "example-output.txt"
 def write_to_output_file(output_file_name, frequent_itemset_dict):
     output = {}
     sorted_output_keys = sorted(
@@ -67,6 +70,7 @@ def write_to_output_file(output_file_name, frequent_itemset_dict):
             output_file.write(f'{item} ({output[item]})\n')
 
 
+# generate C_1 and L_1
 def generate_L_1(dataset, minimum_support_count_threshold, frequent_items_count):
     C_1 = {}
     for i in range(0, len(dataset)):
@@ -75,7 +79,7 @@ def generate_L_1(dataset, minimum_support_count_threshold, frequent_items_count)
                 C_1[dataset[i][j]] = 1
             else:
                 C_1[dataset[i][j]] += 1
-    print("len C_1: ", len(C_1))
+    # print("len C_1: ", len(C_1))
 
     L_1 = []
     for item in C_1:
@@ -86,6 +90,7 @@ def generate_L_1(dataset, minimum_support_count_threshold, frequent_items_count)
     return L_1
 
 
+# generate C_2 and L_2
 def generate_L_2_with_hashtable(dataset, minimum_support_count_threshold, frequent_items_count):
     hash_table = {}
     for transaction in dataset:
@@ -94,7 +99,7 @@ def generate_L_2_with_hashtable(dataset, minimum_support_count_threshold, freque
                 hash_table[itemset] = 1
             else:
                 hash_table[itemset] += 1
-    print("len C_2: ", len(hash_table))
+    # print("len C_2: ", len(hash_table))
 
     L_2 = []
     for itemset in hash_table:
@@ -104,11 +109,14 @@ def generate_L_2_with_hashtable(dataset, minimum_support_count_threshold, freque
     return L_2
 
 
+# generate all the subset from a given set
 def subset_generation(original_set, subset_size):
     return map(list, set(itertools.combinations(original_set, subset_size)))
 
 
+# generate C_L, including joint and prune
 def apriori_gen(L_ksub1, k):
+    # joint process
     C_k = []
     for i in range(len(L_ksub1)):
         for j in range(i+1, len(L_ksub1)):
@@ -117,9 +125,9 @@ def apriori_gen(L_ksub1, k):
             if l1[:k-2] == l2[:k-2]:
                 C_k.append(sorted(list(set(L_ksub1[i]) | set(L_ksub1[j]))))
 
+    # prune process
     pruned_C_k = []
     L_ksub1 = set(L_ksub1)
-
     for itemset in C_k:
         all_subsets = list(subset_generation(set(itemset), k - 1))
         satisfied = True
@@ -131,10 +139,11 @@ def apriori_gen(L_ksub1, k):
         if satisfied == True:
             pruned_C_k.append(tuple(itemset))
 
-    print("len C_{}: {}".format(k, len(pruned_C_k)))
+    # print("len C_{}: {}".format(k, len(pruned_C_k)))
     return pruned_C_k
 
 
+# generate L_k from C_k
 def generate_L_k_from_C_k(dataset, C_k, minimum_support_count_threshold, frequent_items_count):
     L_k = []
     itemset_count = {}
@@ -156,6 +165,7 @@ def generate_L_k_from_C_k(dataset, C_k, minimum_support_count_threshold, frequen
     return L_k
 
 
+# the main apriori algorithm for generate C_1 to C_k, L_1 to L_k and update frequent_items_count
 def run_apriori(argv):
     input_dataset_name = argv[0]
     minimum_support_count_threshold = int(argv[1])
@@ -169,12 +179,12 @@ def run_apriori(argv):
     L_1 = generate_L_1(
         dataset, minimum_support_count_threshold, frequent_items_count)
     L.append(L_1)
-    print("len L_1: ", len(L_1))
+    # print("len L_1: ", len(L_1))
 
     L_2 = generate_L_2_with_hashtable(
         dataset, minimum_support_count_threshold, frequent_items_count)
     L.append(L_2)
-    print("len L_2: ", len(L_2))
+    # print("len L_2: ", len(L_2))
 
     current_L = sorted(L_2)
     k = 3
@@ -183,7 +193,7 @@ def run_apriori(argv):
         C_k = apriori_gen(current_L, k)
         L_k = generate_L_k_from_C_k(
             dataset, C_k, minimum_support_count_threshold, frequent_items_count)
-        print("len L_{}: {}".format(k, len(L_k)))
+        # print("len L_{}: {}".format(k, len(L_k)))
         L.append(L_k)
         k = k + 1
         current_L = sorted(L_k)
